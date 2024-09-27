47768a333bb2ee7863e0c350c2901717bff78155
C4R10SHenrique <car4los007@gmail.com>
2024-09-27 14:38:25 -0500

    Add files via upload

---
+++ estatistica_agentes.py
@@ -0,0 +1,12 @@
+agente = input("Digite o nome do seu agente: ")
+abates =int( input("Digite o seu número de abates: "))
+partidas =int( input("Digite o número de partidas jogadas: "))
+idade =float( input("Digite a idade do agente: "))
+numero_de_abilidades =int( input("Digite o número de abilidades do agente: "))
+abilidades = input("Digite o nome das abilidades separadas por vírgula: ")
+
+texto =f"Meu agente preferido é {agente}, ele tem {idade} anos, e eu já fiz {abates} abates com ele, já joguei {partidas} partidas com ele e seu número de abilidades são {numero_de_abilidades}:{abilidades}."
+print (texto)
+
+
+
